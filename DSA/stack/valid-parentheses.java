e57d8216beeb11e26aec067223634a58432d8123
Saurabh Kumar <89748411+Saurabh-kumar09@users.noreply.github.com>
2023-07-04 23:20:06 +0530

    Create valid-parentheses.java

---
+++ DSA/stack/valid-parentheses.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class Solution {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
