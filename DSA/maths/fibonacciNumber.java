ebe01a27baefd95d4c375d9fd26e7182bf592945
Saurabh Kumar <89748411+Saurabh-kumar09@users.noreply.github.com>
2023-07-16 19:12:01 +0530

    Update fibonacciNumber.java

--- DSA/maths/fibonacciNumber.java
+++ DSA/maths/fibonacciNumber.java
@@ -24,3 +24,19 @@ class Solution {
     }
 } 
 
+
+//Optimized recursive approach
+public class fibonacciNumber{
+    public static void main(String[] args) {
+        System.out.println(fib(0));
+    }
+    static int fib(int n){
+        if(n<2){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+
+
